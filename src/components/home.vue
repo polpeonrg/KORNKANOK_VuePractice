60503821b25a055fec4039a129063b47cf0fa527
polpeonrg <89388396+polpeonrg@users.noreply.github.com>
2022-05-13 15:44:38 +0700

    Initial commit

---
+++ src/components/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <card></card>
+    <card>2nd Card</card>
+  </div>
+</template>
+
+<script>
+import card from "./card";
+
+export default {
+  components: {
+    card,
+  },
+};
+</script>
---
+++ src/components/von.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>V-On</h1>
+    <h2>{{ message }}</h2>
+    <button v-on:click="add()">Add</button>
+    <!-- 
+      <button v-on:click="message = message - 1">Del</button>
+    -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.message = this.message + 1;
+    },
+  },
+};
+</script>
